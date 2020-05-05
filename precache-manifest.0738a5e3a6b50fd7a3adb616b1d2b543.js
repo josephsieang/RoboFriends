49dabba989a9a084fbf99760bf3a4609c3bef2fa
NG HAN SIEANG <josephsieang@NGs-Mac.local>
2020-05-05 13:33:59 +0800

    Updates

---
+++ precache-manifest.0738a5e3a6b50fd7a3adb616b1d2b543.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b469a8198becb4b2d17bfee8668f924",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "e6d023e560c7f690574d",
+    "url": "/RoboFriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "ae36cc2f3ec6c25d477a",
+    "url": "/RoboFriends/static/css/main.9252e209.chunk.css"
+  },
+  {
+    "revision": "e6d023e560c7f690574d",
+    "url": "/RoboFriends/static/js/2.fad0920a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.fad0920a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae36cc2f3ec6c25d477a",
+    "url": "/RoboFriends/static/js/main.7a070871.chunk.js"
+  },
+  {
+    "revision": "9fda7ca17e43078694ea",
+    "url": "/RoboFriends/static/js/runtime-main.7cecb56b.js"
+  },
+  {
+    "revision": "2ca6cac9835098b3eadfb326eee25207",
+    "url": "/RoboFriends/static/media/CocogooseProRegular.2ca6cac9.woff"
+  },
+  {
+    "revision": "6bd85f4b90621400683aa3548f25b6a5",
+    "url": "/RoboFriends/static/media/bg.6bd85f4b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64352d46f577fb368026572152ff6b60.js"
+  "/RoboFriends/precache-manifest.0738a5e3a6b50fd7a3adb616b1d2b543.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboFriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
